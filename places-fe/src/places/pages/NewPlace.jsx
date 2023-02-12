9bf12a8c527a666048248b98b921951bdbed5b12
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2023-02-12 18:46:27 +0100

    removed former build to shift to typescript

--- places-fe/src/places/pages/NewPlace.jsx
+++
@@ -1,5 +0,0 @@
-const NewPlace = () => {
-  return <h2>New Place</h2>;
-};
-
-export default NewPlace;
--- places-fe/src/places/pages/Places.jsx
+++
@@ -1,5 +0,0 @@
-const Places = () => {
-  return <h1>Places</h1>;
-};
-
-export default Places;
