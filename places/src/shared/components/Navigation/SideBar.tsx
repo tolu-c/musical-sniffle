2c61c15fe5e25fc8b6754e83c05db897dffbeb89
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2023-02-13 00:52:34 +0100

    refix sidebar to react portal

--- places/src/shared/components/Navigation/SideBar.tsx
+++ places/src/shared/components/Navigation/SideBar.tsx
@@ -1,9 +1,11 @@
+import { createPortal } from "react-dom";
+import { ReactNode } from "react";
 import Close from "../../../assets/icons/Close";
 import { SideBarProps } from "../../../types/types";
 import NavLinks from "./NavLinks";
 
 const SideBar = ({ onClose }: SideBarProps) => {
-  return (
+  const content: ReactNode = (
     <div className="flex flex-col lg:hidden absolute top-0 right-0 h-screen z-50 w-3/4 md:w-1/2 bg-teal-900">
       <div className="text-white">
         <Close onClick={onClose} size="32" />
@@ -12,6 +14,11 @@ const SideBar = ({ onClose }: SideBarProps) => {
         <NavLinks />
       </nav>
     </div>
+  );
+
+  return createPortal(
+    content,
+    document.getElementById("sidebar") as HTMLElement
   );
 };
 
