a0233ab5a7e66339a0365173b89a0259502812a0
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2023-02-13 01:00:00 +0100

    added overlay to sidebar

--- places/src/shared/components/Navigation/SideBar.tsx
+++ places/src/shared/components/Navigation/SideBar.tsx
@@ -6,13 +6,18 @@ import NavLinks from "./NavLinks";
 
 const SideBar = ({ onClose }: SideBarProps) => {
   const content: ReactNode = (
-    <div className="flex flex-col lg:hidden absolute top-0 right-0 h-screen z-50 w-3/4 md:w-1/2 bg-teal-900">
-      <div className="text-white">
-        <Close onClick={onClose} size="32" />
+    <div
+      className="w-full h-screen bg-teal-900/40 absolute top-0 right-0 z-50"
+      onClick={onClose}
+    >
+      <div className="flex flex-col lg:hidden absolute top-0 right-0 h-screen w-3/4 md:w-1/2 bg-teal-900">
+        <div className="text-white">
+          <Close onClick={onClose} size="32" />
+        </div>
+        <nav className="text-white">
+          <NavLinks />
+        </nav>
       </div>
-      <nav className="text-white">
-        <NavLinks />
-      </nav>
     </div>
   );
 
